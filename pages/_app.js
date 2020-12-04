cc351904e8742aecbd67efbf3b5623c2a620fb7e
Bishoy Romany <7bishoyromany7@gmail.com>
2020-12-04 08:55:09 +0200

    Add: Google Analyser

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,25 @@
 import "../styles/globals.scss";
 import Footer from "@/component/Footer";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-8ZX1KWC4Y5"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-8ZX1KWC4Y5');`,
+          }}
+        ></script>
+      </Head>
       <Component {...pageProps} />
       <Footer />
     </>
